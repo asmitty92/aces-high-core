0ebf858aa5f5063cc569a92cef2445c15b42cbc0
asmitty92 <asmitty92@users.noreply.github.com>
2025-04-29 20:19:59 +0000

    Auto-format changes and new build output

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 // jest.config.js
 export default {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", { useESM: true }],
   },
-  extensionsToTreatAsEsm: ['.ts']
+  extensionsToTreatAsEsm: [".ts"],
 };
